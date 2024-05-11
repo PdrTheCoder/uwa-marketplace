04890b43be2ccf3da1928fd7233a0e3613fb7199
Yunhui <2845317611@qq.com>
2024-05-11 18:51:51 +0800

    combine signup and login to one auth blueprint

--- uwamkp/auth.py
+++ uwamkp/auth.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import flash
+from datetime import datetime,timezone
+from flask import render_template, Blueprint, request, redirect, url_for, flash
 from uwamkp.models import db
 from uwamkp.models import User
-from uwamkp.forms import LoginForm
+from uwamkp.forms import LoginForm,RegistrationForm
 from sqlalchemy import select
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash
+import re  # Regular expression module for validating email and username
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -28,3 +28,27 @@ def login():
                 flash('Wrong email and password combination \
                     or email does not exist', 'danger')
         return render_template("login.html", form=form)
+    
+@bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data.lower(),
+                    username=form.username.data,
+                    password=form.password.data,
+                    created_at=datetime.now(timezone.utc), # timezone-aware UTC datetime
+                    is_admin=False,
+                    deleted=False)
+        db.session.add(user)
+        db.session.commit()
+    return render_template('signup.html', form=form)
+
+
+
