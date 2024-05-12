791708e84620cd7f93a9df6c11ab630504150be8
pedro <paynemax@163.com>
2024-05-12 16:53:05 +0800

    1. add logout function 2. add a constants file 3. add patch function to listings 4. change background color of navbar and footer

--- uwamkp/auth.py
+++ uwamkp/auth.py
@@ -3,10 +3,14 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import flash
+from flask_login import login_required
 from flask_login import login_user
+from flask_login import logout_user
 from uwamkp.models import db
 from uwamkp.models import User
 from uwamkp.forms import LoginForm
+from uwamkp.constants import INDEX_ANONYMOUS
+from uwamkp.constants import INDEX_LOGGED
 from sqlalchemy import select
 
 
@@ -24,9 +28,15 @@ def login():
             user = db.session.scalars(stmt).one_or_none()
             if user and user.verify_password(form.password.data):
                 login_user(user)
-                # TODO later change to main page
-                return redirect('/mylisting/listings')
+                return redirect(INDEX_LOGGED)
             else:
                 flash('Wrong email and password combination \
                     or email does not exist', 'danger')
         return render_template("login.html", form=form)
+
+
+@bp.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(INDEX_ANONYMOUS)
