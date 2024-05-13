f5c9de451af442946d8b0413db62e12e635d4f81
SexyZoe <91857835+SexyZoe@users.noreply.github.com>
2024-05-13 22:49:12 +1000

    Update showcase.js

--- uwamkp/static/js/showcase.js
+++ uwamkp/static/js/showcase.js
@@ -1,11 +1,11 @@
 function searchItems() {
     const query = document.getElementById('search-input').value;
-    // 实现 AJAX 请求，更新商品列表
+    // Implement AJAX request to update product list
     console.log('Searching for:', query);
 }
 
 function sortItems() {
     const sortValue = document.getElementById('sort-select').value;
-    // 实现 AJAX 请求，根据选择的排序选项更新商品列表
+    // Implement an AJAX request to update the product list according to the selected sorting option.
     console.log('Sorting by:', sortValue);
 }
