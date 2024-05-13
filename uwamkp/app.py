b531c44e54206977fa4b6fa045a962151063d32a
pedro <paynemax@163.com>
2024-05-13 14:37:54 +0800

    1. add slash route

--- uwamkp/app.py
+++ uwamkp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask import redirect
 from flask_login import LoginManager
+from flask_login import current_user
 from flask_migrate import Migrate
 import os
 from sqlalchemy import select
@@ -8,6 +10,8 @@ from uwamkp.models import User
 from uwamkp.auth import bp as auth_bp
 from uwamkp.mylisting import bp as mylisting_bp
 from uwamkp.signup import bp as signup_bp
+from uwamkp.constants import INDEX_LOGGED
+from uwamkp.constants import INDEX_ANONYMOUS
 
 
 # create the app and db
@@ -36,3 +40,11 @@ def load_user(user_id):
     stmt = select(User).where(User.id == user_id)
     user = db.session.scalars(stmt).one_or_none()
     return user
+
+
+@app.route("/")
+def slash():
+    if current_user.is_authenticated:
+        return redirect(INDEX_LOGGED)
+    else:
+        return redirect(INDEX_ANONYMOUS)
