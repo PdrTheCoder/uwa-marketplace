2680327a436ec90aef5d3d0a26a76749a5e7b99f
pedro <paynemax@163.com>
2024-05-15 09:32:57 +0800

    fix product detail page

--- uwamkp/models.py
+++ uwamkp/models.py
@@ -9,10 +9,18 @@ from sqlalchemy.dialects.sqlite import (
     VARCHAR,
 )
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from werkzeug.security import check_password_hash
+
+
+def format_iso_datetime(some_datetime):
+    """Format the datetime to isoformat"""
+    return some_datetime.isoformat(sep=" ", timespec="seconds")
 
 
 class Base(DeclarativeBase):
@@ -22,22 +30,29 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
-    salt: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[str] = mapped_column(DATETIME, nullable=False)
     is_admin: Mapped[bool] = mapped_column(BOOLEAN, nullable=False)
     deleted: Mapped[bool] = mapped_column(BOOLEAN, nullable=False)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Listing(Base):
     __tablename__ = 'listing'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(VARCHAR(60), nullable=False)
+    # For condition
+    # 0: New
+    # 1: Used - Like New
+    # 2: Used - Good
+    # 3: Used - Fair
     condition: Mapped[int] = mapped_column(SMALLINT, nullable=False)
     price: Mapped[float] = mapped_column(DECIMAL, nullable=False)
     description: Mapped[str] = mapped_column(TEXT, nullable=False)
@@ -48,9 +63,34 @@ class Listing(Base):
     sold: Mapped[bool] = mapped_column(BOOLEAN, nullable=False)
     deleted: Mapped[bool] = mapped_column(BOOLEAN, nullable=False)
     created_at: Mapped[str] = mapped_column(DATETIME, nullable=False)
-    updated_at: Mapped[str] = mapped_column(DATETIME)
+    updated_at: Mapped[str] = mapped_column(DATETIME, nullable=True)
     category_id: Mapped[int] = mapped_column(
-        ForeignKey('category.id', ondelete='SET NULL'))
+        ForeignKey('category.id', ondelete='SET NULL'), nullable=True)
+    seller = relationship("User")
+
+    def to_dict(self):
+        condition_mapping = {
+            0: "New",
+            1: "Used - Like New",
+            2: "Used - Good",
+            3: "Used - Fair"
+        }
+        return {
+            "id": self.id,
+            "title": self.title,
+            "condition": condition_mapping[self.condition],
+            "price": round(self.price, 2),
+            "description": self.description,
+            "seller_id": self.seller_id,
+            "seller_name": self.seller.username,
+            "suspended": self.suspended,
+            "sold": self.sold,
+            "deleted": self.deleted,
+            "created_at": format_iso_datetime(
+                self.created_at),
+            "updated_at": format_iso_datetime(
+                self.updated_at) if self.updated_at else None
+        }
 
 
 class Reply(Base):
--- uwamkp/products.py
+++ uwamkp/products.py
@@ -2,49 +2,47 @@ from flask import Blueprint
 from flask import request
 from flask import render_template
 from flask import flash
-from uwamkp import app
 from uwamkp.models import db
 from uwamkp.models import Listing
-from uwamkp.models import Base
-from uwamkp.models import User
 
 bp = Blueprint('product', __name__)
 
 # todo:login required
-@bp.route('/addproduct', methods=['GET','POST'])
-
-def addproduct():
-    # username = current_user.username
-    # email = current_user.email
-    # created_at = current_user.created_at.isoformat(sep=" ", timespec="seconds")
-
-    if request.method == 'POST':
-        title = request.form.get('productTitle')
-        price = request.form.get('productPrice')
-        condition = request.form.get('productCondition')
-        description = request.form.get('productDescription')
-
-        try: 
-            new_listing = Listing(title=title, 
-                                price=float(price), 
-                                condition=condition, 
-                                description=description
-                                )
-            db.session.add(new_listing)
-            db.session.commit()
-            flash('Your product has been added!')
-            return redirect(url_for('details',id=new_listing.id))
-        except Exception as e:
-            db.session.rollback()
-            flash('Error adding product.')
-            return render_template("addproduct.html")
-    return render_template("addproduct.html")
 
 
-@bp.route('/details/<int:id>',methods=['GET','POST'])
-def details(id):
-    product = Listing.query.get(id)
-    return render_template('details.html')
+# @bp.route('/addproduct', methods=['GET', 'POST'])
+# def addproduct():
+#     # username = current_user.username
+#     # email = current_user.email
+#     # created_at = current_user.created_at.isoformat(sep=" ", timespec="seconds")
+
+#     if request.method == 'POST':
+#         title = request.form.get('productTitle')
+#         price = request.form.get('productPrice')
+#         condition = request.form.get('productCondition')
+#         description = request.form.get('productDescription')
+
+#         try:
+#             new_listing = Listing(title=title,
+#                                   price=float(price),
+#                                   condition=condition,
+#                                   description=description
+#                                   )
+#             db.session.add(new_listing)
+#             db.session.commit()
+#             flash('Your product has been added!')
+#             return redirect(url_for('details', id=new_listing.id))
+#         except Exception as e:
+#             db.session.rollback()
+#             flash('Error adding product.')
+#             return render_template("addproduct.html")
+#     return render_template("addproduct.html")
+
+
+@bp.route('/details/<int:listing_id>', methods=['GET', 'POST'])
+def details(listing_id):
+    listing = db.session.query(Listing).get(listing_id)
+    return render_template('details.html', listing=listing.to_dict())
 
 # @bp.route('/details/<int:id>',methods=['GET','POST'])
 # def details(id):
