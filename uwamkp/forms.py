88a0f4fc219fed198dc7bd73627479c9a9ebebc9
Yunhui <2845317611@qq.com>
2024-05-17 19:37:07 +0800

    add Email Valiator and toggle password visibility function
    --email validation: must be 8 digits followed by @student.uwa.edu.au;
    -- Implemented toggle password visibility function in `signup.html` and `login.html`:
    - Added buttons within the password input fields to toggle the visibility of the password.
      - The text button switches between 'Show' and 'Hide' to indicate the current state of the password field on signup.html.
    - The icon button switches between 'fa-eye-slash' and 'fa-eye' to indicate the current state of the password field on login.html.

--- uwamkp/forms.py
+++ uwamkp/forms.py
@@ -17,7 +17,9 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+                                             Email(),
+                                             Regexp(r'^\d{8}@student\.uwa\.edu\.au$', 0, 'Email must be 8 digits followed by @student.uwa.edu.au')
+                                             ])
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
@@ -34,8 +36,8 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data.lower()).first():
-            raise ValidationError('Email already registered.')
-
+            raise ValidationError('Email already registered.') 
+    
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
