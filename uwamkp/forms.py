d72b438065d8a6a0f0c506df598cc912a54b1f1d
Yunhui Zhang <132897066+YunhuiZ@users.noreply.github.com>
2024-05-18 01:48:17 +0800

    Toggle password visibility (#37)
    * Add Password strength
    
    -Length Validator: Ensures the password is at least 8 characters long.
    -Regexp Validators: Adds regular expression checks for:
      At least one lowercase letter.
      At least one uppercase letter.
      At least one digit.
      At least one special character.
    
    * add Email Valiator and toggle password visibility function
    
    --email validation: must be 8 digits followed by @student.uwa.edu.au;
    -- Implemented toggle password visibility function in `signup.html` and `login.html`:
    - Added buttons within the password input fields to toggle the visibility of the password.
      - The text button switches between 'Show' and 'Hide' to indicate the current state of the password field on signup.html.
    - The icon button switches between 'fa-eye-slash' and 'fa-eye' to indicate the current state of the password field on login.html.

--- uwamkp/forms.py
+++ uwamkp/forms.py
@@ -14,23 +14,30 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     # TODO remember_me = BooleanField('Keep me logged in')
-    
+
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+                                             Email(),
+                                             Regexp(r'^\d{8}@student\.uwa\.edu\.au$', 0, 'Email must be 8 digits followed by @student.uwa.edu.au')
+                                             ])
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                'Username must start with a letter and include only letters, numbers, dots or underscores')])
     password = PasswordField('Password', validators=[
-        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+        DataRequired(), EqualTo('password2', message='Passwords must match.'),
+        Length(min=8, message='Password must be at least 8 characters long.'),
+        Regexp('(?=.*[a-z])', 0, 'Password must contain at least one lowercase letter.'),
+        Regexp('(?=.*[A-Z])', 0, 'Password must contain at least one uppercase letter.'),
+        Regexp('(?=.*[0-9])', 0, 'Password must contain at least one digit.'),
+        Regexp('(?=.*[!@#$%^&*(),.?":{}|<>])', 0, 'Password must contain at least one special character.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data.lower()).first():
-            raise ValidationError('Email already registered.')
-
+            raise ValidationError('Email already registered.') 
+    
     def validate_username(self, field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
